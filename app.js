ec0d5998109b0928fc61f82a09cc6096db1aa1ad
safiullah <Safiullah55555>
2024-11-12 20:54:10 +0500

    responsiveness added

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    // dropdown handle 
+    
+    function toggleDropdown(){
+        const dropdownMenu=document.getElementById("dropdownMenu")
+        dropdownMenu.style.display=dropdownMenu.style.display=="block"?"none":"block";
+    }
+
+    window.onclick=function(event){
+        if(!event.target.matches(".icon")){
+            const dropdowns=document.getElementsByClassName("dropdown-content")
+            for (let i=0; i<dropdowns.length;i++){
+                const openDropdown=dropdowns[i]
+                if(openDropdown.style.display=="block"){
+                    openDropdown.style.display="none"
+                }
+            }
+        }
+    }
